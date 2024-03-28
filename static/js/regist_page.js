453a29851fbf546d2ae8a4879a8a554561021db2
AlexNuman <fast_sco@mail.ru>
2024-03-28 18:14:36 +0600

    'Fix-28-03-24'

--- static/js/regist_page.js
+++ static/js/regist_page.js
@@ -40,37 +40,78 @@ if ($('#user_type_label').text()=='Кабинет администратора')
 /*---------------Проверка логина------------------------------------------*/
 $('#login_regist').keyup(function() {
   var type_login = $('#login_regist').val();
+  var check_login;
   if (type_login.length < 4 || type_login.length > 20) {
-    $('#login_len').css({
-    'color': '#A60800',
-    'opacity': '0.7'});
+    $('#login_regist').css({
+    'background-image': 'url("/static/img/cancel.png")',
+    'background-repeat': 'no-repeat',
+    'padding-right': '5px',
+    'background-size': '20px 20px',
+    'background-position': 'right',
+    'transition': 'background-image 0.25s ease-in-out 0.1s'});
+    check_login = '0';
   } else {
-    $('#login_len').css({
-    'color': '#1E786C',
-    'opacity': '1.0'});
+    $('#login_regist').css({
+    'background-image': 'url("/static/img/ok.png")',
+    'background-repeat': 'no-repeat',
+    'padding-right': '5px',
+    'background-size': '20px 20px',
+    'background-position': 'right',
+    'transition': 'background-image 0.25s ease-in-out 0.1s'});
+    check_login = '1';
+  };
+  if (type_login === "") {
+    $("#login_regist").css("background-image", "none");
   };
 });
 /*-------------------------------------------------------------------------*/
 /*---------------Проверка пароля ------------------------------------------*/
 $('#pass').keyup(function(){
   var type_pass = $('#pass').val();
-  if (type_pass.length < 4 || type_pass.length > 20) {
-    $('#pass_len').css({
-    'color': '#A60800',
-    'opacity': '0.7'});
-  } else {
-    $('#pass_len').css({
-    'color': '#1E786C',
-    'opacity': '1.0'});
+  var check_pass;
+  if (isStrongPassword(type_pass) == true && type_pass.length >= 8) {
+    $('#pass').css({
+    'background-image': 'url("/static/img/ok.png")',
+    'background-repeat': 'no-repeat',
+    'padding-right': '5px',
+    'background-size': '20px 20px',
+    'background-position': 'right',
+    'transition': 'background-image 0.25s ease-in-out 0.1s'});
+    check_pass = 1;
+    if (check_login == '1') {
+      $('#button_regist').removeAttr('disabled');
+    };
+  } else if (isStrongPassword(type_pass) == false && type_pass.length < 8) {
+    $('#pass').css({
+    'background-image': 'url("/static/img/cancel.png")',
+    'background-repeat': 'no-repeat',
+    'padding-right': '5px',
+    'background-size': '20px 20px',
+    'background-position': 'right',
+    'transition': 'background-image 0.25s ease-in-out 0.1s'});
+    check_pass = 0;
+    $('#button_regist').attr('disabled', 'true');
   };
-  if (!/[A-Z]/.test(type_pass) || !/\d/.test(type_pass)) {
-    $('#pass_len').css({
-    'color': '#A60800',
-    'opacity': '0.7'});
-} else {
-    $('#pass_len').css({
-    'color': '#1E786C',
-    'opacity': '1.0'});
-}
+  if (type_pass === "") {
+    $("#pass").css("background-image", "none");
+    $('#button_regist').attr('disabled', 'true');
+  };
 });
+
 /*-------------------------------------------------------------------------*/
+function isStrongPassword(password) {
+    // Проверка наличия хотя бы одной цифры
+  const hasDigit = /[0-9]/.test(password);
+  // Проверка наличия хотя бы одной буквы в верхнем регистре
+  const hasUppercase = /[A-Z]/.test(password);
+  // Проверка наличия хотя бы одной буквы в нижнем регистре
+  const hasLowercase = /[a-z]/.test(password);
+  // Проверка наличия хотя бы одного специального символа
+  const hasSpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\|]/.test(password);
+
+  // Пароль считается сильным, если выполняются все условия
+  return hasDigit && hasUppercase && hasLowercase && hasSpecialChar;
+};
+
+
+
